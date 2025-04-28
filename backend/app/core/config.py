9512759abd42939ebf035d30906dbfa83a000983
sgshn4 <davidov.oleg2018@yandex.ru>
2025-04-28 21:40:36 +0300

    refactor: переписан механизм конструкции переменной для подключения к БД.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.caqcjpanlhmeloojgpcz:9afrlC3PR90C2yNU@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
