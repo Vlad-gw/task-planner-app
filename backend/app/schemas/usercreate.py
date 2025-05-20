e344d37729ec309ab43d7600f4bdcb107229b17c
Arlequinn <alexkmv22@gmail.com>
2025-05-21 00:11:42 +0300

    fix: Удаление неиспользуемых импортов и исправление паттернов

--- backend/app/schemas/usercreate.py
+++ backend/app/schemas/usercreate.py
@@ -3,13 +3,15 @@ from typing import Optional
 
 
 class UserCreate(BaseModel):
-    first_name: str = Field(..., min_length=1, max_length=20)
-    second_name: str = Field(..., min_length=1, max_length=50)
+    first_name: str = Field(..., min_length=1, max_length=20, pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+                            description="Имя пользователя")
+    second_name: str = Field(..., min_length=1, max_length=50, pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+                             description="Фамилия пользователя")
     email: EmailStr
     password: str = Field(..., min_length=6)
     role: str = "user"
-    icon: Optional[int] = 1
     is_verified: bool = False
+    icon: Optional[int] = 1
 
     class Config:
         from_attributes = True
