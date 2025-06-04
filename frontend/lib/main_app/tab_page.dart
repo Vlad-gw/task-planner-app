1b08366a85fefd787944d1228ed35f914424315e
sgshn4 <davidov.oleg2018@yandex.ru>
2025-06-04 04:01:26 +0300

    feat: Добавлен кастомный элемент списка для отображения задач.

--- frontend/lib/main_app/tab_page.dart
+++ frontend/lib/main_app/tab_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:punctualis_1/structures/task.dart';
+import 'package:punctualis_1/widgets/tab_page_list_item.dart';
 class TabPage extends StatelessWidget {
   final String title;
   final int count;
@@ -12,8 +14,14 @@ class TabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(child: Text('Count: $count')),
+      body: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (context, index) {
+          return TabPageListItem(
+           task: Task(title: 'lolo', description: 'description', priority: 1, creationDate: 15645, finishDate: 4546456, isDone: true, timeReminder: 4564564, scheduledAt: 44846)
+          );
+        },
+      ),
     );
   }
 }
